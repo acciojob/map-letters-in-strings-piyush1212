e29f61a413ed65f9148c07922a28584f2643c217
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-03-29 13:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function mapLetters(word) {
+  const letterIndexes = {};
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    if (!letterIndexes[letter]) {
+      letterIndexes[letter] = [];
+    }
+    letterIndexes[letter].push(i);
+  }
+  return letterIndexes;
+}
